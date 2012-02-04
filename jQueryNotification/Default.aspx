b8cf437b4a00d9b96c97db4d33ad218cae3c3e0c
Mohamed Meligy <eng.meligy@gmail.com>
2012-02-04 22:45:00 +1100

    Added the sample user control to the page and finished the example of multiple controls notifications calls showing in controls rendering order. Note all the notifications will still come at the top of the page though.

--- jQueryNotification/Default.aspx
+++ jQueryNotification/Default.aspx
@@ -1,5 +1,7 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="jQueryNotification.Default" %>
 
+<%@ Register src="SampleUserControl.ascx" tagPrefix="controls" tagName="SampleUserControl" %>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -35,6 +37,9 @@
             Text="Delayed Error Notification" onclick="btnDelayedError_Click" />
 
     </div>
+    
+    <controls:SampleUserControl runat="server" ID="sampleUserControl" />
+
     </form>
 </body>
 </html>
---
+++ jQueryNotification/SampleUserControl.ascx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using JqueryNotification;
+
+namespace jQueryNotification
+{
+    public partial class SampleUserControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnShowUserControlNotification_Click(object sender, EventArgs e)
+        {
+            // Note using <br /> in the message to show you can use HTML in there
+            // Of course if you are using HTML in real cases, avoid creating it by using strings like this
+
+            // Note using 'this' indicating the call belongs to the user control
+            this.ShowSuccessfulNotification("Called first, but shown second<br />As I belong to some control on the page");
+            // Note using 'Page' indicating the call belongs to the Page itself
+            Page.ShowWarningNotification("Called second, but shown first<br />As I belong to the page itself");
+        }
+    }
+}
